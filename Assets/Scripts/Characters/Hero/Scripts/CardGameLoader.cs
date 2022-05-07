925a438db19f4c59aed1daf437f13e867958cbc2
oleja <noilzep@gmail.com>
2022-05-07 22:34:42 +0300

    Inport CardGame
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

---
+++ Assets/Scripts/Characters/Hero/Scripts/CardGameLoader.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CardGameLoader : MonoBehaviour
+{
+    [SerializeField] private Camera hero_camera;
+    [SerializeField] private Camera card_camera;
+    [SerializeField] private Camera Ui_camera;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            hero_camera.enabled = false;
+            Ui_camera.enabled = false;
+            card_camera.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Ui_camera.enabled = true;
+            card_camera.enabled = false;
+            hero_camera.enabled = true;
+        }
+    }
+}
