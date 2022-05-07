21198b2e31576fff21877c9ec1f2aa7b00218e5f
oleja <noilzep@gmail.com>
2022-05-07 23:17:16 +0300

    little card update
    Co-Authored-By: lfybbk10 <63192458+lfybbk10@users.noreply.github.com>

--- Assets/Scripts/Characters/Hero/Scripts/CardGameLoader.cs
+++ Assets/Scripts/Characters/Hero/Scripts/CardGameLoader.cs
@@ -9,36 +9,16 @@ public class CardGameLoader : MonoBehaviour
     [SerializeField] private GameObject Ui_camera;
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.F9))
         {
             Ui_camera.SetActive(false);
             card_camera.SetActive(true);
         }
         else if (Input.GetKeyDown(KeyCode.Backspace))
-        {
+        {   
             Ui_camera.SetActive(true);
             card_camera.SetActive(false);
         }
     }
 }
 
-/*
-using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript : MonoBehaviour {
-public Camera[] playercam;
-int i=0;
-int dlina;
-        void Update () {
-                if (Input.GetKeyDown("c")) {
-                                i=i+1;
-                                        if (i >= dlina){i=0;}}
-                dlina=playercam.Length;
-                playercam[i].enabled = true;
-                playercam[i-1].enabled = false;
-                        if (i==0)
-                        playercam[dlina].enabled = false;
-        }
-}
-*/
