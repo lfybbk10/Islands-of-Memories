b39a0af52a103e013af6c62d4d69061faa776646
lfybbk10 <daniil.sindler@gmail.com>
2022-05-10 18:54:38 +0300

    rework fight system

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,62 +4,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
 public class Enemy : MonoBehaviour, IDamageable
 {
     [SerializeField] private Flicker _flicker;
     [SerializeField] private float _health;
-    [SerializeField] private Animator _animator;
-    [SerializeField] private LayerMask _hero;
+    [SerializeField] private LayerMask _heroMask;
+    private Animator _animator;
     private NavMeshAgent _agent;
     private Collider[] _colliders = new Collider[50];
     public Action Dead { get; set; }
 
+    [Header("Combat")] 
+    [SerializeField] private float _attack = 3f;
+    [SerializeField] private float _range = 1f;
+    [SerializeField] private float _aggroRange = 4f;
+
+    private float _timePassed;
+    private Hero _hero;
+
     private void Awake()
     {
+        
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         Dead += OnDead;
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
-        // _agent.SetDestination(RuntimeContext.Instance.hero.transform.position);
-        // _animator.SetFloat("Forward", 0.9f, 0.1f, Time.deltaTime);
+        _hero = RuntimeContext.Instance.hero;
     }
 
     private void OnDead()
     {
-        
     }
 
     public float Value
     {
         get => _health;
-        set
-        {
-            Dead?.Invoke();
-            _health = value;
-        }
+        set => _health = value;
     }
 
     public void TakeDamage()
     {
-        var count = Physics.OverlapSphereNonAlloc(transform.position, 10f, _colliders,_hero);
+        var count = Physics.OverlapSphereNonAlloc(transform.position, 10f, _colliders, _heroMask);
         if (count == 0)
             return;
         _colliders[0].GetComponent<IDamageable>().ApplyDamage(1);
     }
-    
-    
+
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", _agent.velocity.magnitude / _agent.speed);
+        if (_timePassed >= _attack)
+        {
+            if (Vector3.Distance(_hero.transform.position, transform.position) <= _range)
+            {
+                _animator.SetTrigger("Attack");
+                _timePassed = 0;
+            }
+        }
+        _timePassed += Time.deltaTime;
+        if (Vector3.Distance(_hero.transform.position, transform.position) <= _aggroRange)
+        {
+            _agent.SetDestination(_hero.transform.position);
+        }
+        transform.LookAt(_hero.transform);
+    }
+
+
     public void ApplyDamage(float value)
     {
         Value -= value;
-        _flicker.Create();
+        if (Value < 0)
+            Destroy(gameObject);
+        _animator.SetTrigger("Damage");
+        //_flicker.Create();
     }
 
     private void OnDestroy()
     {
         Dead -= OnDead;
+    }
+
+    private void OnDrawGizmos()
+    {   
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,_range);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,_aggroRange);
+        
     }
 }
